47accb97967f4ba3daeed410ebad2374c49621f9
manuel aullo <manuel.aullo@cybercom.com>
2020-10-05 09:53:39 +0300

    Updates

---
+++ precache-manifest.734218b71bf9be612f9a7b031057b0d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ae19c0d4d4c491b673455ae6383e76",
+    "url": "/react-monster-filtering/index.html"
+  },
+  {
+    "revision": "54d8e6e9461ba3a224a5",
+    "url": "/react-monster-filtering/static/css/main.a7509683.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/react-monster-filtering/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-filtering/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d8e6e9461ba3a224a5",
+    "url": "/react-monster-filtering/static/js/main.958087d0.chunk.js"
+  },
+  {
+    "revision": "b8edd7c54f81ad6efebc",
+    "url": "/react-monster-filtering/static/js/runtime-main.ebf71f87.js"
+  }
+]);
